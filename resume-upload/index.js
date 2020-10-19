563244f9775a880b05911fd4267b878f9bf8e8d4
蔡胜龙 <1187023819@qq.com>
2020-10-19 11:36:16 +0800

    aaa

---
+++ resume-upload/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: SailorCai
+ * @Date: 2020-10-19 11:34:38
+ * @LastEditors: SailorCai
+ * @LastEditTime: 2020-10-19 11:35:04
+ * @FilePath: /resume-upload/index.js
+ */
+module.exports = function() {
+  console.log('this is resume upload plugin');
+};
